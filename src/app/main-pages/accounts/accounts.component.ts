6cff946888e62272c5eabd5d9bdd3adf20f319aa
Karol Ciak <ciakkarol@hotmail.com>
2023-06-26 11:22:08 +0200

    add animation, edit accounts

--- src/app/main-pages/accounts/accounts.component.ts
+++ src/app/main-pages/accounts/accounts.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
+import {
+  fadeInRightBigOnEnterAnimation,
+  slideOutDownOnLeaveAnimation,
+} from 'angular-animations';
 
 @Component({
   selector: 'app-accounts',
   templateUrl: './accounts.component.html',
   styleUrls: ['./accounts.component.scss'],
+  animations: [
+    fadeInRightBigOnEnterAnimation(),
+    slideOutDownOnLeaveAnimation({ translate: '1000px' }),
+    ,
+  ],
 })
-export class AccountsComponent {
-  componentActif = 'accounts';
+export class AccountsComponent implements AfterViewInit {
+  // componentActif = 'accounts';
+  toggleAnimation: boolean = true;
+  animationDisabled: boolean = true;
+  test = true;
+
+  toggleAnim() {
+    this.toggleAnimation = !this.toggleAnimation;
+  }
+
+  ngAfterViewInit(): void {
+    setTimeout(() => {
+      this.animationDisabled = false;
+      this.test = false;
+    }, 0);
+  }
 }
